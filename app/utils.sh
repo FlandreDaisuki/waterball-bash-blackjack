90bb5862f1eed013048661eecab2f1b03f9b43a1
FlandreDaisuki <vbnm123c+github@gmail.com>
2024-03-30 16:56:15 +0800

    feat: support 2 kind of frontend

--- app/utils.sh
+++ app/utils.sh
@@ -7,3 +7,7 @@ function new_shuffled_deck {
 function init_players_from_payload {
   echo "${1}" | jq -cr '.players | map(.+={cards:[],chips:100})'
 }
+
+function is_browser_user_agent {
+  echo "${1}" | grep -q 'Mozilla'
+}
---
+++ app/views/frontend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo 'Welcome to Waterball Bash Blackjack!'
